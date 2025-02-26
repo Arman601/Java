dd0fac6d28c33dcb45279a4f6bcc62550de5befe
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-02-26 15:42:56 +0530

    Add files via upload

---
+++ AvarageCalculated.java
@@ -0,0 +1,9 @@
+public class AvarageCalculated{
+    public static void main(String args[]){
+    double sum = 10+20+30+40+50;
+        int count = 5;
+        System.out.println(sum/count);
+
+}
+
+}
