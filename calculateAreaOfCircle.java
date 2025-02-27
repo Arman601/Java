4d6b020e7ec3f715de7945af48abb1cad1603b8d
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-02-27 14:53:40 +0530

    Add files via upload

---
+++ calculateAreaOfCircle.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class PracticeQ{
+    public static void main(String args[]){
+       //write a program to calculate area of circle
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the area");
+        float rad = sc.nextFloat();  //Area of circle  formula =  ( pi*r square )
+
+       float area =  3.14f *rad*rad;
+       System.out.println(area);
+    
+
+
+    }
+}
+
