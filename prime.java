17656b65abab466f474b1a6207af7b40885ca443
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-03-04 13:34:10 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class prime{
+
+    public static void main(String args[]){
+    System.out.println("Enter a:");
+    Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if( a%2 == 0){
+            System.out.println("this is a prime number");
+        }
+        else {
+            System.out.println("Not Prime");
+
+        }
+       
+    }
+}
+
+
+
+
+
